7bff5f7769e30a17282716d307fb8832c1f6e43b
dxiDavid <davidgichuruofficial@gmail.com>
2023-03-25 16:27:05 +0300

    Project setup

---
+++ results-summary-component-main/src/Results.jsx
@@ -0,0 +1,11 @@
+
+function result() {
+
+  return (
+    <div className="App">
+      <h1>component 1</h1>
+    </div>
+  )
+}
+
+export default result;
---
+++ results-summary-component-main/src/Summary.jsx
@@ -0,0 +1,10 @@
+
+function summary(){
+    return(
+      <div className="App">
+       <p>component 2</p>
+      </div>
+    )
+  }
+
+export default summary  
