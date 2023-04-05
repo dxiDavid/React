0a4c51c281a980de3a5ee4414935aa67040ac888
dxiDavid <davidgichuruofficial@gmail.com>
2023-04-06 00:19:53 +0300

    Project setup and start

---
+++ notifications-page/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Notification from './notification-component'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Notification />
+  </React.StrictMode>,
+)
