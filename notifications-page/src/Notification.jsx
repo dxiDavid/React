1a6e7b28790033909014186ff4a3ad2538875e54
dxiDavid <davidgichuruofficial@gmail.com>
2023-04-09 00:25:52 +0300

    Rendered all the notifications conditionally

--- notifications-page/src/Notification.jsx
+++ notifications-page/src/Notification.jsx
@@ -1,4 +1,3 @@
-
 import data from "./data"
 
 function Notification_Template(props){
@@ -6,28 +5,34 @@ function Notification_Template(props){
   return(
       <div className="notification-container">
         <div className="profile-picture">
-          <img src={props.profile_picture} alt="profile-picture"/>
+          <img src={ props.profile_picture } alt="profile-picture"/>
         </div>
         <div className="notification-content">
           <div className="notification-details">
             <div className="profile-name">
-              <p>{props.name}</p>
+              <p>{ props.name }</p>
             </div>
             <div className="notification-type">
-              {/*Conditional rendering for the notification type*/}
-              <p>{props.notificationType}</p>
+              {props.notificationType.isReaction ===true && <p>reacted to your recent post {props.notificationReference}</p>}
+              {props.notificationType.isNewFollower ===true && <p>followed you</p>}
+              {props.notificationType.isJoinedARelatedGroup ===true && <p>joined your group {props.group}</p>}
+              {props.notificationType.isLeftRelatedGroup ===true && <p>left the group {props.group}</p>}
+              {props.notificationType.isPrivateMesage ===true && 
+                <>
+                  <p>sent you a private message</p> 
+                  <div className="private-message"><p>{props.message}</p></div>
+                </>}
+              {props.notificationType.isComment ===true && <p>commented on your picture</p>}
             </div>
             <div className="notification-reference">
               {/*Implement conditional rendering for image reference*/}
-              <p>{props.notificationReference}</p>
+              <p>{ }</p>
               <div className="red-dot"></div>
             </div>
           </div>
           <div className="notification-time">
             <p>
-              {/*write a function that calculates 
-              the amount of time that has passed since 
-              the notification was shared*/}
+                {props.howLongAgo}
             </p>
           </div>
         </div>
@@ -40,10 +45,13 @@ function singleNotification() {
   const dataElements = data.map( notification_item => {
     return <Notification_Template 
 
-              name = {notification_item.name}
-              notificationType = {notification_item.notificationType}  
-              notificationReference = {notification_item.notificationReference}
-              profile_picture = {notification_item.profile_picture}
+              name = { notification_item.name }
+              notificationType = { notification_item.notificationType }  
+              notificationReference = { notification_item.notificationReference }
+              profile_picture = { notification_item.profile_picture }
+              //message = { notification_item.message }
+              group = { notification_item.group }
+              howLongAgo = { notification_item.howLongAgo }
               />
   } )
 
--- notifications-page/src/data.jsx
+++ notifications-page/src/data.jsx
@@ -4,9 +4,142 @@ export default [
         id: 1,
         name: "Mark Webber",
         profile_picture: "/src/assets/avatar-mark-webber.webp",
-        notificationType: "reacted to your recent post",
-        notificationReference: "My first tournament today"
+        group: null,
+        message:null,
+        notificationType: {
+            isReaction: true,
+            isPrivateMesage: false,
+            isComment: false,
+            isReply: false,
+            isNewFollower: false,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: false,
+            isLeftRelatedGroup: false,
+            isRead: false
+        },
+        notificationReference: "My first tournament today",
+        howLongAgo: "1m ago"
+    },
+    {
+        id: 2,
+        name: "Angela Gray",
+        profile_picture: "/src/assets/avatar-angela-gray.webp",
+        group: null,
+        message:null,
+        notificationType: {
+            isReaction: false,
+            isPrivateMesage: false,
+            isComment: false,
+            isReply: false,
+            isNewFollower: true,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: false,
+            isLeftRelatedGroup: false,
+            isRead: false
+        },
+        notificationReference: null,
+        howLongAgo: "5m ago"
+    },
+    {
+        id: 3,
+        name: "Jacob Thompson",
+        profile_picture: "/src/assets/avatar-jacob-thompson.webp",
+        group: "Chess Club",
+        message:null,
+        notificationType: {
+            isReaction: false,
+            isPrivateMesage: false,
+            isComment: false,
+            isReply: false,
+            isNewFollower: false,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: true,
+            isLeftRelatedGroup: false,
+            isRead: false
+        },
+        notificationReference: "My first tournament today",
+        howLongAgo: "1 day ago"
+    },
+    {
+        id: 4,
+        name: " Rizky Hasanuddin",
+        profile_picture: "/src/assets/avatar-rizky-hasanuddin.webp",
+        group: null,
+        message:`   Hello, thanks for setting up the Chess Club. 
+                    I've been a member for a few weeks now and \n 
+                    I'm already having lots of fun and improving my game.\n`,
+        notificationType: {
+            isReaction: false,
+            isPrivateMesage: true,
+            isComment: false,
+            isReply: false,
+            isNewFollower: false,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: false,
+            isLeftRelatedGroup: false,
+            isRead: false
+        },
+        notificationReference: null,
+        howLongAgo: "5 days ago"
+    },
+    {
+        id: 5,
+        name: "Kimberly Smith",
+        profile_picture: "/src/assets/avatar-kimberly-smith.webp",
+        group: null,
+        message:null,
+        notificationType: {
+            isReaction: false,
+            isPrivateMesage: false,
+            isComment: true,
+            isReply: false,
+            isNewFollower: false,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: false,
+            isLeftRelatedGroup: false,
+            isRead: false
+        },
+        notificationReference: "picture path",
+        howLongAgo: "1 week ago"
+    },
+    {
+        id: 6,
+        name: "Nathan Peterson",
+        profile_picture: "/src/assets/avatar-nathan-peterson.webp",
+        group: null,
+        message:null,
+        notificationType: {
+            isReaction: true,
+            isPrivateMesage: false,
+            isComment: false,
+            isReply: false,
+            isNewFollower: false,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: false,
+            isLeftRelatedGroup: false,
+            isRead: false
+        },
+        notificationReference: "end-game strategies to increase your win rate",
+        howLongAgo: "2 weeks ago"
+    },
+    {
+        id: 7,
+        name: "Anna Kim",
+        profile_picture: "/src/assets/avatar-anna-kim.webp",
+        group: "Chess Club",
+        message:null,
+        notificationType: {
+            isReaction: false,
+            isPrivateMesage: false,
+            isComment: false,
+            isReply: false,
+            isNewFollower: false,
+            isUnfollowedUser: false,
+            isJoinedARelatedGroup: false,
+            isLeftRelatedGroup: true,
+            isRead: false
+        },
+        notificationReference: null,
+        howLongAgo: "2 weeks ago"
     },
 ]
-
-//set isReaction, isPrivateMesage, isReply, hasJoinedARelatedGroup, hasLeftRelatedGroup, variables on the objects and use those props to render conditionally.
