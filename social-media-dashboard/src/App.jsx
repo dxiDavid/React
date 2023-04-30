9616f4e5437dcc0d72668b3589ee8bfbecf689b4
dxiDavid <davidgichuruofficial@gmail.com>
2023-04-30 22:21:45 +0300

    Project setup

---
+++ social-media-dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import data from "./data"
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ social-media-dashboard/src/data.jsx
@@ -0,0 +1,5 @@
+export default [
+    {
+        value: 1
+    }
+]
