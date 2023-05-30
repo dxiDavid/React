ed4729f6275e39899738d7aa66c79a9b930412a3
David Gichuru <davidgichuruofficial@gmail.com>
2023-05-30 12:46:27 +0300

    Removed browser router

--- react-router-practice/src/main.jsx
+++ react-router-practice/src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import App from './App.jsx'
-import Projects from './Components/Projects.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-      </Routes>
-    </BrowserRouter>
+    <App/>
   </React.StrictMode>,
 )
