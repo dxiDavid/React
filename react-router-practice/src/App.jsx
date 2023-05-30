44c4a54072d02a9d011d48631de8a7d29b32511f
David Gichuru <davidgichuruofficial@gmail.com>
2023-05-30 12:45:06 +0300

    Made the App component the parent

--- react-router-practice/src/App.jsx
+++ react-router-practice/src/App.jsx
@@ -1,21 +1,32 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Nav from './Components/Nav'
+import Home from './Components/Home'
+import Projects from './Components/Projects.jsx'
+import About from './Components/About'
 
 function App() {
-  const [isClicked, setIsClicked] = useState(false)
+  // const [isClicked, setIsClicked] = useState(false)
 
-  const toggle = () =>{
-    setIsClicked((prev) => !prev)}
+  // const toggle = () =>{
+  //   setIsClicked((prev) => !prev)
+  // }
 
   return (
-    <>
-    <Nav
-      toggleBurger={isClicked ? "nav-toggle burger-active" : "nav-toggle "}
-      toggleLinks={isClicked ? "links links-active" : "links"}
-      handleClick = {() => toggle()}
-    />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+
+// {/* <Nav
+//       toggleBurger={isClicked ? "nav-toggle burger-active" : "nav-toggle "}
+//       toggleLinks={isClicked ? "links links-active" : "links"}
+//       handleClick = {() => toggle()}
+// /> */}
