76dcaf7030e91dab278624ee7a6269bc202b5ae9
David Gichuru <davidgichuruofficial@gmail.com>
2023-05-30 12:47:46 +0300

    Created file

---
+++ react-router-practice/src/Components/albums.js
@@ -0,0 +1,42 @@
+export default albums[
+    {
+        id: 1,
+        image: "./Cover art/2.png",
+    },
+    {
+        id: 2,
+        image: "./Cover art/3.png",
+    },
+    {
+        id: 3,
+        image: "./Cover art/4.png",
+    },
+    {
+        id: 4,
+        image: "./Cover art/5.png",
+    },
+    {
+        id: 5,
+        image: "./Cover art/6.png",
+    },
+    {
+        id: 6,
+        image: "./Cover art/7.png",
+    },
+    {
+        id: 7,
+        image: "./Cover art/8.png",
+    },
+    {
+        id: 8,
+        image: "./Cover art/9.png",
+    },
+    {
+        id: 9,
+        image: "./Cover art/10.png",
+    },
+    {
+        id: 10,
+        image: "./Cover art/11.png",
+    }
+]
---
+++ react-router-practice/src/Components/laws.js
@@ -0,0 +1,245 @@
+export default [
+
+        {
+            id: 1,
+            image: "./48 laws of power/Law 1.png",
+            law: "NEVER OUTSHINE THE MASTER"
+        },
+        {
+            id: 2,
+            image: "./48 laws of power/Law 2.png",
+            law: "NEVER OUTSHINE THE MASTER"
+        },
+        {
+            id: 3,
+            image: "./48 laws of power/Law 3.png",
+            law: "CONCEAL YOUR INTENTIONS"
+        },
+        {
+            id: 4,
+            image: "./48 laws of power/Law 4.png",
+            law: "ALWAYS SAY LESS THAN NECESSARY"
+        },
+        {
+            id: 5,
+            image: "./48 laws of power/Law 5.png",
+            law: "SO MUCH DEPENDS ON REPUTATION – GUARD IT WITH YOUR LIFE."
+        },
+        {
+            id: 6,
+            image: "./48 laws of power/Law 6.png",
+            law: "COURT ATTENTION AT ALL COST"
+        },
+        {
+            id: 7,
+            image: "./48 laws of power/Law 7.png",
+            law: "GET OTHERS TO DO THE WORK FOR YOU, BUT ALWAYS TAKE THE CREDIT"
+        },
+        {
+            id: 8,
+            image: "./48 laws of power/Law 8.png",
+            law: "MAKE OTHER PEOPLE COME TO YOU – USE BAIT IF NECESSARY"
+        },
+        {
+            id: 9,
+            image: "./48 laws of power/Law 9.png",
+            law: "WIN THROUGH YOUR ACTIONS, NEVER THROUGH ARGUMENT"
+        },
+        {
+            id: 10,
+            image: "./48 laws of power/Law 10.png",
+            law: "INFECTION: AVOID THE UNHAPPY AND THE UNLUCKY"
+        },
+        {
+            id: 11,
+            image: "./48 laws of power/Law 11.png",
+            law: "LEARN TO KEEP PEOPLE DEPENDENT ON YOU"
+        },
+        {
+            id: 12,
+            image: "./48 laws of power/Law 12.png",
+            law: "USE SELECTIVE HONESTY AND GENEROSITY TO DISARM YOUR VICTIM"
+        },
+        {
+            id: 13,
+            image: "./48 laws of power/Law 13.png",
+            law: "WHEN ASKING FOR HELP, APPEAL TO PEOPLE’S SELF-INTEREST, NEVER TO THEIR MERCY OR GRATITUDE"
+        },
+        {
+            id: 14,
+            image: "./48 laws of power/Law 14.png",
+            law: "POSE AS A FRIEND, WORK AS A SPY"
+        },
+        {
+            id: 15,
+            image: "./48 laws of power/Law 15.png",
+            law: "CRUSH YOUR ENEMY TOTALLY"
+        },
+        {
+            id: 16,
+            image: "./48 laws of power/Law 16.png",
+            law: "USE ABSENCE TO INCREASE RESPECT AND HONOR"
+        },
+        {
+            id: 17,
+            image: "./48 laws of power/Law 17.png",
+            law: "KEEP OTHERS IN SUSPENDED TERROR: CULTIVATE AN AIR OF UNPREDICTABILITY"
+        },
+        {
+            id: 18,
+            image: "./48 laws of power/Law 18.png",
+            law: "DO NOT BUILD FORTRESSES TO PROTECT YOURSELF – ISOLATION IS DANGEROUS"
+        },
+        {
+            id: 19,
+            image: "./48 laws of power/Law 19.png",
+            law: "KNOW WHO YOU’RE DEALING WITH – DO NOT OFFEND THE WRONG PERSON"
+        },
+        {
+            id: 20,
+            image: "./48 laws of power/Law 20.png",
+            law: "DO NOT COMMIT TO ANYONE"
+        },
+        {
+            id: 21,
+            image: "./48 laws of power/Law 21.png",
+            law: "PLAY A SUCKER TO CATCH A SUCKER – SEEM DUMBER THAN YOUR MARK"
+        },
+        {
+            id: 22,
+            image: "./48 laws of power/Law 22.png",
+            law: "USE THE SURRENDER TACTIC: TRANSFORM WEAKNESS INTO POWER"
+        },
+        {
+            id: 23,
+            image: "./48 laws of power/Law 23.png",
+            law: "CONCENTRATE YOUR FORCES"
+        },
+        {
+            id: 24,
+            image: "./48 laws of power/Law 24.png",
+            law: "PLAY THE PERFECT COURTIER"
+        },
+        {
+            id: 25,
+            image: "./48 laws of power/Law 25.png",
+            law: "RE-CREATE YOURSELF"
+        },
+        {
+            id: 26,
+            image: "./48 laws of power/Law 26.png",
+            law: "KEEP YOUR HANDS CLEAN"
+        },
+        {
+            id: 27,
+            image: "./48 laws of power/Law 27.png",
+            law: "PLAY ON PEOPLE’S NEED TO BELIEVE TO CREATE A CULT-LIKE FOLLOWING"
+        },
+        {
+            id: 28,
+            image: "./48 laws of power/Law 28.png",
+            law: "ENTER ACTION WITH BOLDNESS"
+        },
+        {
+            id: 29,
+            image: "./48 laws of power/Law 29.png",
+            law: "PLAN ALL THE WAY TO THE END"
+        },
+        {
+            id: 30,
+            image: "./48 laws of power/Law 30.png",
+            law: "MAKE YOUR ACCOMPLISHMENTS SEEM EFFORTLESS"
+        },
+        {
+            id: 31,
+            image: "./48 laws of power/Law 31.png",
+            law: "CONTROL THE OPTIONS: GET OTHERS TO PLAY WITH THE CARDS YOU DEAL"
+        },
+        {
+            id: 32,
+            image: "./48 laws of power/Law 32.png",
+            law: "CONTROL THE OPTIONS: GET OTHERS TO PLAY WITH THE CARDS YOU DEAL"
+        },
+        {
+            id: 33,
+            image: "./48 laws of power/Law 33.png",
+            law: "DISCOVER EACH MAN’S THUMBSCREW"
+        },
+        {
+            id: 34,
+            image: "./48 laws of power/Law 34.png",
+            law: "BE ROYAL IN YOUR OWN FASHION: ACT LIKE A KING TO BE TREATED LIKE ONE"
+        },
+        {
+            id: 35,
+            image: "./48 laws of power/Law 35.png",
+            law: "MASTER THE ART OF TIMING"
+        },
+        {
+            id: 36,
+            image: "./48 laws of power/Law 36.png",
+            law: "DISDAIN THINGS YOU CANNOT HAVE: IGNORING THEM IS THE BEST REVENGE"
+        },
+        {
+            id: 37,
+            image: "./48 laws of power/Law 37.png",
+            law: "CREATE COMPELLING SPECTACLES"
+        },
+        {
+            id: 38,
+            image: "./48 laws of power/Law 38.png",
+            law: "CREATE COMPELLING SPECTACLES"
+        },
+        {
+            id: 39,
+            image: "./48 laws of power/Law 39.png",
+            law: "STIR UP WATERS TO CATCH FISH"
+        },
+        {
+            id: 40,
+            image: "./48 laws of power/Law 40.png",
+            law: "DESPISE THE FREE LUNCH"
+        },
+        {
+            id: 41,
+            image: "./48 laws of power/Law 41.png",
+            law: "AVOID STEPPING INTO A GREAT MAN’S SHOES"
+        },
+        {
+            id: 42,
+            image: "./48 laws of power/Law 42.png",
+            law: "STRIKE THE SHEPHERD AND THE SHEEP WILL SCATTER"
+        },
+        {
+            id: 43,
+            image: "./48 laws of power/Law 43.png",
+            law: "WORK ON THE HEARTS AND MINDS OF OTHERS"
+        },
+        {
+            id: 44,
+            image: "./48 laws of power/Law 44.png",
+            law: "DISARM AND INFURIATE WITH THE MIRROR EFFECT"
+        },
+        {
+            id: 45,
+            image: "./48 laws of power/Law 45.png",
+            law: "PREACH THE NEED FOR CHANGE, BUT NEVER REFORM TOO MUCH AT ONCE"
+        },
+        {
+            id: 46,
+            image: "./48 laws of power/Law 46.png",
+            law: "NEVER APPEAR TOO PERFECT"
+        },
+        {
+            id: 47,
+            image: "./48 laws of power/Law 47.png",
+            law: "DO NOT GO PAST THE MARK YOU AIMED FOR; IN VICTORY, LEARN WHEN TO STOP"
+        },
+        {
+            id: 48,
+            image: "./48 laws of power/Law 48.png",
+            law: "ASSUME FORMLESSNESS"
+        }
+    ]
+
+
